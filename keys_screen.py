41c8ed2d7313f51a17e2a5972d88992c9fee8f1d
mixl5448 <mixl5448@gmail.com>
2020-04-29 23:01:15 -0700

    Added functionality to print names of keydown

--- keys_screen.py
+++ keys_screen.py
@@ -7,6 +7,8 @@ class KeysScreen:
 		pygame.init()
 		self.settings = Settings()
 		self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+		self.font = pygame.font.Font(self.settings.font_string, self.settings.font_size)
+		self.text = None
 		pygame.display.set_caption("Keys")
 
 	def run_game(self):
@@ -18,9 +20,19 @@ class KeysScreen:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT: 
 				sys.exit()
+			if event.type == pygame.KEYDOWN: 
+				self._print_to_screen(event)
+
+	def _print_to_screen(self, event): 
+		self.text = self.font.render(pygame.key.name(event.key) , True, (0,0,0), (255,255,255))
+
 
 	def _update_screen(self): 
 		self.screen.fill(self.settings.bg_color)
+		if self.text != None :
+			text_rect = self.text.get_rect()
+			text_rect.center = self.screen.get_rect().center
+			self.screen.blit(self.text, text_rect)
 		pygame.display.flip()
 
 if __name__ == "__main__": 
--- settings.py
+++ settings.py
@@ -3,3 +3,5 @@ class Settings:
 		self.screen_width = 800
 		self.screen_height = 500
 		self.bg_color = (255,255,255)
+		self.font_string = 'freesansbold.ttf'
+		self.font_size = 32
